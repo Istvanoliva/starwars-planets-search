a98bbc9c2baf94d5bf3288f5a5f6336bd4f8f38a
Istvan <olivaistvan@gmail.com>
2022-03-06 00:32:43 -0300

    Requisitos 03 e 04

--- src/components/SearchPlanets.jsx
+++ src/components/SearchPlanets.jsx
@@ -2,18 +2,68 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 const SearchPlanets = () => {
-  const { setFilter } = useContext(PlanetsContext);
+  const { setFilter, setColumn, setComparison, setNumber,
+    filterByNumericValues: { column, number, comparison },
+    updateFilter, data } = useContext(PlanetsContext);
 
   return (
-    <label htmlFor="filterPlanet">
-      <input
-        id="filterPlanet"
-        type="text"
-        data-testid="name-filter"
-        placeholder="Search..."
-        onChange={ setFilter }
-      />
-    </label>
+    <form>
+      <label htmlFor="filterPlanet">
+        <input
+          id="filterPlanet"
+          type="text"
+          data-testid="name-filter"
+          placeholder="Search..."
+          onChange={ setFilter }
+        />
+      </label>
+      <label htmlFor="column">
+        <select
+          name="column"
+          id="column"
+          data-testid="column-filter"
+          value={ column }
+          onChange={ ({ target: { value } }) => setColumn(value) }
+        >
+          <option>population</option>
+          <option>orbital_period</option>
+          <option>diameter</option>
+          <option>rotation_period</option>
+          <option>surface_water</option>
+        </select>
+      </label>
+      <label htmlFor="comparison">
+        <select
+          name="comparison"
+          id="comparison"
+          data-testid="comparison-filter"
+          value={ comparison }
+          onChange={ ({ target: { value } }) => setComparison(value) }
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+      </label>
+      <label htmlFor="value">
+        <input
+          id="value"
+          name="number"
+          type="number"
+          data-testid="value-filter"
+          placeholder="Search..."
+          value={ number }
+          onChange={ ({ target: { value } }) => setNumber(value) }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ () => updateFilter(data) }
+      >
+        Filter
+      </button>
+    </form>
   );
 };
 
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -6,7 +6,11 @@ import fetchPlanetsList from '../Services/fetchPlanets';
 const PlanetsProvider = ({ children }) => {
   const [planets, setPlanets] = useState([]);
   const [filterPlanets, setFilterPlanets] = useState({ filterByName: { name: '' } });
+  const [column, setColumn] = useState('population');
+  const [comparison, setComparison] = useState('maior que');
+  const [number, setNumber] = useState(0);
 
+  // Requisito 01 - Fetch Planets
   const getPlanets = async () => {
     const response = await fetchPlanetsList();
     setPlanets(response.results);
@@ -16,19 +20,39 @@ const PlanetsProvider = ({ children }) => {
     getPlanets();
   }, []);
 
+  // Requisito 02 - Filter Name
   const setFilter = ({ target }) => {
     setFilterPlanets({ filterByName: { name: target.value } });
   };
 
-  const value = {
+  // Requisito 03 e 04 - Filter Numeric
+  const updateFilter = (array) => {
+    const result = array.filter((planet) => {
+      if (comparison === 'maior que') {
+        return Number(planet[column]) > Number(number);
+      }
+      if (comparison === 'menor que') {
+        return Number(planet[column]) < Number(number);
+      }
+      return Number(planet[column]) === Number(number);
+    });
+    setPlanets(result);
+  };
+
+  const STATE = {
     data: planets,
     setPlanets,
     filterPlanets,
     setFilter,
+    updateFilter,
+    setColumn,
+    setComparison,
+    setNumber,
+    filterByNumericValues: { column, comparison, number },
   };
 
   return (
-    <PlanetsContext.Provider value={ value }>
+    <PlanetsContext.Provider value={ STATE }>
       { children }
     </PlanetsContext.Provider>
   );
